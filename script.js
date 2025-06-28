ddc497c32ae2b009abd4e1bc7dde4443fc63310f
kokoab <batiancelafrancis747@gmail.com>
2025-06-28 13:24:29 +0800

    made button and defined book class

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const myLibrary = [];
 
 function Book(){
-
+    this.title = '';
+    this.author = '';
+    this.pages = 0;
+    this.read = false;
 }
 
 function addBooktoLibrary(title, author, pages, read) {
